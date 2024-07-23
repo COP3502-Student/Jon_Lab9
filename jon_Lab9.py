491d2d9f38eb234114a8345d99b718257470f626
COP3502-Student <161520540+COP3502-Student@users.noreply.github.com>
2024-07-23 15:20:33 -0400

    added main function

--- jon_Lab9.py
+++ jon_Lab9.py
@@ -15,3 +15,28 @@ def decode(password):
     decoded_pass = ''.join(nums)
     return decoded_pass
 
+def main():
+    encoded_password = None
+    while True:
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        option = input("Please enter an option: ")
+
+        if option == '1':
+            password = input("Please enter the password to encode: ")
+            encoded_password = encode(password)
+            print("Your password has been encoded and stored! \n")
+        elif option == '2':
+            if encoded_password:
+                decoded_password = decode(encoded_password)
+                print(f"The encoded password is {encoded_password}, and the original password is {decoded_password}. \n")
+            else:
+                print("No encoded password stored. Please encode a password first.")
+        elif option == '3':
+            break
+
+if __name__ == "__main__":
+    main()
